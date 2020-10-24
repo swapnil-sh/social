67bcd6fa11e16a6dfe5607272dad32ce8371baf6
swapnil-sh <swapnil.sharma2709@gmail.com>
2020-10-25 00:11:49 +0530

    project setup

---
+++ src/bffbook/po.md
@@ -0,0 +1,12 @@
+There will be 3 applications in this project
+
+1. Profiles
+    -- Profile (Model) : will use django signals
+    -- Relatiosnhip (model) : will help us manage         relations (Sender,Receiver,Status of relationship)
+
+2. Posts
+    -- Posts(model)
+    -- Comment(model)
+    -- Like(model)
+    
+3. allauth (authentication)
---
+++ src/profiles/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Profile(models.Model):
+
+    first_name = models.CharField(max_length=200, blank=True)
+    last_name = models.CharField(max_length=200, blank=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(default="no bio.....", max_length=300)
+    email = models.EmailField(max_length=200, blank=True)
+    country = models.CharField(max_length=200, blank=True)
+    avatar = models.ImageField(default='avatar.png', upload_to='')
+    #install pillow
+    friends
+    slug
+    updated
+    created
