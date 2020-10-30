7a4e3bb5ac7a74af090185d1f8162a57444e7da3
swapnil-sh <swapnil.sharma2709@gmail.com>
2020-10-31 02:30:35 +0530

    Added Profile Card and Profile Update Modal

--- src/profiles/views.py
+++ src/profiles/views.py
@@ -1,14 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Profile
+from .forms import ProfileModelForm
 
 # Create your views here.
 
 def my_profile_view(request):
 
     profile = Profile.objects.get(user=request.user)
+    form = ProfileModelForm(request.POST or None, request.FILES or None, instance=profile)
+    confirm = False
 
+    if(request.method == 'POST'):
+        if(form.is_valid()):
+            form.save()
+            confirm = True
+            
     context = {
         'profile':profile,
+        'form':form,
+        'confirm':confirm,
     }
     return render(request, 'profiles/myprofile.html', context)
--- src/static_project/main.js
+++ src/static_project/main.js
@@ -1,4 +1,8 @@
 $(document).ready(function(){
 
-    console.log('working from the base')
+    $('#modal-btn').click(function(){
+        $('.ui.modal')
+        .modal('show');
+
+    })
 })
