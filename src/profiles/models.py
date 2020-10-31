5f558b9a4f517683d4436feebd32baf11b4c77ab
swapnil-sh <swapnil.sharma2709@gmail.com>
2020-10-31 13:52:27 +0530

    Created models posts,likes and comments, accessed data through forward and backward relationships and displayed data

--- src/profiles/models.py
+++ src/profiles/models.py
@@ -23,6 +23,29 @@ class Profile(models.Model):
     def get_friends_no(self):
         return self.friends.all().count()
 
+    def get_posts_no(self):
+        return self.posts.all().count()
+
+    def get_all_authors_posts(self):
+        return self.posts.all()
+    
+    def get_likes_given_no(self):
+        likes = self.like_set.all()
+        total_liked = 0
+        for item in likes:
+            if(item.value == 'Like'):
+                total_liked+=1
+        
+        return total_liked
+
+    def get_likes_recieved_no(self):
+        posts = self.posts.all()
+        total_liked = 0
+        for item in posts:
+            total_liked += item.liked.all().count()
+        
+        return total_liked
+
     def __str__(self):
         return f"{self.user.username}-{self.created.strftime('%d-%m-%Y')}"
 
