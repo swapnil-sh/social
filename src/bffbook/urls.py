2996408734d95d791342e8f9a3bc391ae656645e
swapnil-sh <swapnil.sharma2709@gmail.com>
2020-10-31 18:44:26 +0530

    Listing out the posts, created a view that will handle liking and unliking posts and created a toggle Like button

--- src/bffbook/urls.py
+++ src/bffbook/urls.py
@@ -22,7 +22,8 @@ from .views import home_view
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_view, name='home-view'),
-    path('profiles/', include('profiles.urls', namespace='profiles'))
+    path('profiles/', include('profiles.urls', namespace='profiles')),
+    path('posts/', include('posts.urls', namespace='posts')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
