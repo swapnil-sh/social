64fafe22f04542650f4adc903eb55cd94835897a
swapnil-sh <swapnil.sharma2709@gmail.com>
2020-11-01 14:09:58 +0530

    getting invitations from different users, getting all profiles in the system excluding ourselves and getting all the profiles that we can invite to friends

--- src/profiles/models.py
+++ src/profiles/models.py
@@ -2,6 +2,30 @@ from django.db import models
 from django.contrib.auth.models import User
 from .utils import get_random_code
 from django.template.defaultfilters import slugify
+from django.db.models import Q
+
+class ProfileManager(models.Manager):
+
+    def get_all_profiles_to_invite(self, sender):
+        profiles = Profile.objects.all().exclude(user=sender)
+        profile = Profile.objects.get(user=sender)
+        qs = Relationship.objects.filter(Q(sender=profile) | Q(receiver=profile))
+
+        accepted = set([])
+        for rel in qs:
+            if(rel.status == 'accepted'):
+                 accepted.add(rel.receiver)
+                 accepted.add(rel.sender)
+
+        available = [profile for profile in profiles if profile not in accepted]
+        print(available)
+        return available
+
+    def get_all_profiles(self, me):
+        profiles = Profile.objects.all().exclude(user=me)
+        return profiles
+
+
 
 class Profile(models.Model):
 
@@ -16,6 +40,8 @@ class Profile(models.Model):
     slug = models.SlugField(unique=True, blank=True)
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
+
+    objects = ProfileManager()
 
     def get_friends(self):
         return self.friends.all()
@@ -71,12 +97,21 @@ STATUS_CHOICES = (
     ('accepted', 'accepted')
 )
 
+class RelationshipManager(models.Manager):
+    def invitations_received(self, receiver):
+        qs = Relationship.objects.filter(receiver=receiver, status='send')
+        return qs
+
+   # Relationship.objects.invitations_received(myprofile)
+
 class Relationship(models.Model):
     sender = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='sender')
     receiver = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='receiver')
     status = models.CharField(max_length=8, choices=STATUS_CHOICES)
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
+
+    objects = RelationshipManager()
 
     def __str__(self):
         return f"{self.sender}-{self.receiver}-{self.status}"
--- src/profiles/views.py
+++ src/profiles/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Profile
+from .models import Profile, Relationship
 from .forms import ProfileModelForm
 
 # Create your views here.
@@ -22,3 +22,34 @@ def my_profile_view(request):
         'confirm':confirm,
     }
     return render(request, 'profiles/myprofile.html', context)
+
+def invites_received_view(request):
+    profile = Profile.objects.get(user=request.user)
+    qs = Relationship.objects.invitations_received(profile)
+
+    context = {
+        'qs':qs,
+    }
+
+    return render(request, 'profiles/my_invites.html', context)
+
+def profiles_list_view(request):
+    user = request.user
+    qs = Profile.objects.get_all_profiles(user)
+
+    context = {
+        'qs':qs,
+    }
+
+    return render(request, 'profiles/profile_list.html', context)
+
+
+def invite_profiles_list_view(request):
+    user = request.user
+    qs = Profile.objects.get_all_profiles_to_invite(user)
+
+    context = {
+        'qs':qs,
+    }
+
+    return render(request, 'profiles/to_invite_list.html', context)
