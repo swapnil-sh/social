e990fab71033b7dd02c0a1d542d3229eb0aacdd5
swapnil-sh <swapnil.sharma2709@gmail.com>
2020-11-03 02:08:51 +0530

    Refactoring function view to a classed based view for profiles and overriding get_queryset method and workiing with get_context_data

--- src/profiles/urls.py
+++ src/profiles/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import my_profile_view, invites_received_view, profiles_list_view, invite_profiles_list_view
+from .views import ProfileListView, my_profile_view, invites_received_view, profiles_list_view, invite_profiles_list_view
 
 app_name = 'profiles'
 
@@ -7,7 +7,7 @@ urlpatterns = [
 
     path('myprofile/', my_profile_view, name='my-profile-view'),
     path('my-invites/', invites_received_view, name='my-invites-view'),
-    path('all-profiles/', profiles_list_view, name='all-profiles-view'),
+    path('all-profiles/', ProfileListView.as_view(), name='all-profiles-view'),
     path('to-invite/', invite_profiles_list_view, name='invite-profiles-view'),
 
 ]
--- src/profiles/views.py
+++ src/profiles/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Profile, Relationship
 from .forms import ProfileModelForm
+from django.views.generic import ListView
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -33,16 +35,6 @@ def invites_received_view(request):
 
     return render(request, 'profiles/my_invites.html', context)
 
-def profiles_list_view(request):
-    user = request.user
-    qs = Profile.objects.get_all_profiles(user)
-
-    context = {
-        'qs':qs,
-    }
-
-    return render(request, 'profiles/profile_list.html', context)
-
 
 def invite_profiles_list_view(request):
     user = request.user
@@ -53,3 +45,48 @@ def invite_profiles_list_view(request):
     }
 
     return render(request, 'profiles/to_invite_list.html', context)
+
+
+def profiles_list_view(request):
+    user = request.user
+    qs = Profile.objects.get_all_profiles(user)
+
+    context = {
+        'qs':qs,
+    }
+
+    return render(request, 'profiles/profile_list.html', context)
+
+class ProfileListView(ListView):
+    model = Profile
+    template_name = 'profiles/profile_list.html'
+    #context_object_name = 'qs'
+
+    def get_queryset(self):
+        qs = Profile.objects.get_all_profiles(self.request.user)
+        return qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        user = User.objects.get(username__iexact=self.request.user)
+        profile = Profile.objects.get(user=user)
+        rel_r = Relationship.objects.filter(sender=profile)
+        rel_s = Relationship.objects.filter(receiver=profile)
+        rel_receiver = []
+        rel_sender = []
+        for item in rel_r:
+            rel_receiver.append(item.receiver.user)
+
+        for item in rel_s:
+            rel_sender.append(item.sender.user)
+
+        context["rel_receiver"] = rel_receiver
+        context["rel_sender"] = rel_sender
+        context["is_empty"] = False
+
+        if(len(self.get_queryset()) == 0):
+            context["is_empty"] = True
+           
+
+        return  context
+
