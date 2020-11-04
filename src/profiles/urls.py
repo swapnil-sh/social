9c20a418c84945bd0dd4ac0f35526bb730882900
swapnil-sh <swapnil.sharma2709@gmail.com>
2020-11-05 01:54:05 +0530

    listing out senders of invites, accepting invites and rejecting invites

--- src/profiles/urls.py
+++ src/profiles/urls.py
@@ -7,6 +7,8 @@ from .views import (
     invite_profiles_list_view,
     send_invitation,
     remove_from_friends,
+    accept_invitation,
+    reject_invitation,
 )
 
 app_name = 'profiles'
@@ -19,4 +21,6 @@ urlpatterns = [
     path('to-invite/', invite_profiles_list_view, name='invite-profiles-view'),
     path('send-invite/', send_invitation, name='send-invite'),
     path('remove-friend/', remove_from_friends, name='remove-friend'),
+    path('my-invites/accept/', accept_invitation, name='accept-invite'),
+    path('my-invites/reject/', reject_invitation, name='reject-invite'),
 ]
